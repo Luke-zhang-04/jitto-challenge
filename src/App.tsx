02275cf81c8a1f3962c8aeecfb8e95d69668070d
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2023-10-21 10:52:28 -0400

    feat: add scaling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export const App: React.FC = () => {
     return (
         <div className={styles.contentContainer}>
             <BarGraph
-                data={[5, 10, 15, 20]}
+                data={[5, 10, 15, 20, 3, 40, 50, 2, 3, 5, 100, 2, 3, 4, 5, 6, 7, 8, 10]}
                 labels={["label 3", "label 4", "label 2", "label 1"]}
             />
         </div>
--- src/components/bar-graph/index.tsx
+++ src/components/bar-graph/index.tsx
@@ -8,15 +8,39 @@ export interface BarGraphProps {
 const width = 750
 const height = 500
 const axisPadding = 50
-const topPadding = 50
+const topPadding = 20
 
 export const BarGraph: React.FC<BarGraphProps> = ({data}) => {
     const max = Math.max(...data)
     const min = Math.min(...data)
-    const barWidth = (width - axisPadding) / data.length
+    let barSpacing = (width - axisPadding) / (data.length + 1) / data.length
+
+    // For large amounts of data, remove spaces between bars
+    if (barSpacing < 5) {
+        barSpacing = 0
+    }
+
+    const barWidth = (width - axisPadding - barSpacing) / data.length - barSpacing
 
     return (
         <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
+            {data.map((value, index) => {
+                const barHeight = (height - axisPadding - topPadding) * (value / max)
+                const barX =
+                    axisPadding +
+                    barSpacing +
+                    ((width - axisPadding - barSpacing) / data.length) * index
+
+                return (
+                    <rect
+                        x={barX}
+                        y={height - barHeight - axisPadding}
+                        width={barWidth}
+                        height={barHeight}
+                        className={styles.bar}
+                    ></rect>
+                )
+            })}
             <line
                 x1={axisPadding}
                 y1={0}
@@ -31,20 +55,6 @@ export const BarGraph: React.FC<BarGraphProps> = ({data}) => {
                 y2={height - axisPadding}
                 className={styles.axis}
             ></line>
-            {data.map((value, index) => {
-                const barHeight = (height - axisPadding - topPadding) * (value / max)
-                const barX = axisPadding + ((width - axisPadding) / data.length) * index
-
-                return (
-                    <rect
-                        x={barX}
-                        y={height - barHeight - axisPadding}
-                        width={barWidth}
-                        height={barHeight}
-                        className={styles.bar}
-                    ></rect>
-                )
-            })}
         </svg>
     )
 }
