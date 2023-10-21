fa8ae437cff98c84232b6555bf266d314d616fb2
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2023-10-21 11:33:27 -0400

    feat: add labels

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import {BarGraph} from "./components/bar-graph"
 import styles from "./App.module.scss"
 
+const data = [5, 10, 15, 20, 3, 40, 50, 2, 3, 5, 105, 2, 3, 4, 5, 6, 7, 8, 10]
+
 export const App: React.FC = () => {
     return (
         <div className={styles.contentContainer}>
-            <BarGraph
-                data={[5, 10, 15, 20, 3, 40, 50, 2, 3, 5, 100, 2, 3, 4, 5, 6, 7, 8, 10]}
-                labels={["label 3", "label 4", "label 2", "label 1"]}
-            />
+            <BarGraph data={data} labels={data.map((_, index) => `Label ${index}`)} />
         </div>
     )
 }
--- src/components/bar-graph/index.tsx
+++ src/components/bar-graph/index.tsx
@@ -8,18 +8,19 @@ export interface BarGraphProps {
 
 const width = 750
 const height = 500
-const axisPadding = 50
+const xAxisPadding = 50
+const yAxisPadding = 100
 const topPadding = 20
 const labelAxisPadding = 10
 
-export const BarGraph: React.FC<BarGraphProps> = ({data}) => {
+export const BarGraph: React.FC<BarGraphProps> = ({data, labels: xLabels}) => {
     const max = Math.max(...data)
     const barSpacing = (() => {
-        const spacing = (width - axisPadding) / (data.length + 1) / data.length
+        const spacing = (width - xAxisPadding) / (data.length + 1) / data.length
 
         return spacing < 5 ? 0 : spacing
     })()
-    const barWidth = (width - axisPadding - barSpacing) / data.length - barSpacing
+    const barWidth = (width - xAxisPadding - barSpacing) / data.length - barSpacing
     const yLabelCount = Math.min(Math.max(5, max / 10), 10)
 
     // TODO: make labels nice whole numbers
@@ -27,12 +28,12 @@ export const BarGraph: React.FC<BarGraphProps> = ({data}) => {
         .fill(undefined)
         .map((_, index) => (max / 10) * (index + 1))
 
-    const yLabelSpacing = (height - axisPadding - topPadding) / yLabelCount
+    const yLabelSpacing = (height - yAxisPadding - topPadding) / yLabelCount
 
     return (
         <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
             {yLabels.map((label, index) => {
-                const labelY = height - axisPadding - yLabelSpacing * (1 + index)
+                const labelY = height - yAxisPadding - yLabelSpacing * (1 + index)
 
                 return (
                     <React.Fragment key={`yLabel-${label}`}>
@@ -40,20 +41,20 @@ export const BarGraph: React.FC<BarGraphProps> = ({data}) => {
                             className={styles.axisLabel}
                             textAnchor="end"
                             dominantBaseline="middle"
-                            x={axisPadding - labelAxisPadding}
+                            x={xAxisPadding - labelAxisPadding}
                             y={labelY}
                         >
                             {label}
                         </text>
                         <line
-                            x1={axisPadding}
+                            x1={xAxisPadding}
                             y1={labelY}
                             x2={width}
                             y2={labelY}
                             className={styles.gridLine}
                         ></line>
                         <line
-                            x1={axisPadding}
+                            x1={xAxisPadding}
                             y1={labelY + yLabelSpacing / 2}
                             x2={width}
                             y2={labelY + yLabelSpacing / 2}
@@ -63,35 +64,56 @@ export const BarGraph: React.FC<BarGraphProps> = ({data}) => {
                 )
             })}
             {data.map((value, index) => {
-                const barHeight = (height - axisPadding - topPadding) * (value / max)
+                const barHeight = (height - yAxisPadding - topPadding) * (value / max)
                 const barX =
-                    axisPadding +
+                    xAxisPadding +
                     barSpacing +
-                    ((width - axisPadding - barSpacing) / data.length) * index
+                    ((width - xAxisPadding - barSpacing) / data.length) * index
 
                 return (
                     <rect
                         key={`bar-${value}-${index}`}
                         x={barX}
-                        y={height - barHeight - axisPadding}
+                        y={height - barHeight - yAxisPadding}
                         width={barWidth}
                         height={barHeight}
                         className={styles.bar}
                     ></rect>
                 )
             })}
+            {/* Will always display labels if they are specified, no matter the number of bars. Assumes that the number of labels = length of data. */}
+            {xLabels?.map((label, index) => {
+                const labelX =
+                    xAxisPadding +
+                    barSpacing +
+                    barWidth / 2 +
+                    ((width - xAxisPadding - barSpacing) / data.length) * index
+                const labelY = height - yAxisPadding + labelAxisPadding
+
+                return (
+                    <text
+                        className={styles.axisLabel}
+                        textAnchor="end"
+                        x={labelX}
+                        y={labelY}
+                        transform={`rotate(270, ${labelX}, ${labelY})`}
+                    >
+                        {label}
+                    </text>
+                )
+            }) ?? []}
             <line
-                x1={axisPadding}
+                x1={xAxisPadding}
                 y1={0}
-                x2={axisPadding}
+                x2={xAxisPadding}
                 y2={width}
                 className={styles.axis}
             ></line>
             <line
                 x1={0}
-                y1={height - axisPadding}
+                y1={height - yAxisPadding}
                 x2={width}
-                y2={height - axisPadding}
+                y2={height - yAxisPadding}
                 className={styles.axis}
             ></line>
         </svg>
