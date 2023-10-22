bc8e6a5847d833ed70a1ffde9baf584a3712b16c
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2023-10-21 21:06:24 -0400

    fix: update title

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 
-/**
- * @param no Bruh
- */
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <App />
